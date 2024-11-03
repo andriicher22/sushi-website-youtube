bcd1dc3b340e52d379e2740d2f99350a24cbeea9
Andrii <Andrii Cherednichenko>
2024-11-03 14:59:38 -0600

    added card change sctip

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,29 @@
-
 const radioButtons = document.querySelectorAll('input[name="food-filter"]');
+const cards = document.querySelectorAll(".popular-foods__card");
 
 //Change the group when the radiobutton is clicked
-radioButtons.forEach(radioButton => {
-  radioButton.addEventListener('change', () => {
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", () => {
     if (radioButton.checked) {
-      console.log('Selected value:', radioButton.value);
+      console.log("Selected value:", radioButton.value);
       changeActiveGroup();
     }
   });
 });
 
+function changeActiveGroup() {}
 
-function changeActiveGroup(){};
+// Adding the active class to the clicked card
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    console.log("Card was clicked");
+    removeActiveAll();
+    card.classList.add("active-card");
+  });
+});
+
+function removeActiveAll() {
+  cards.forEach((card) => {
+    card.classList.remove("active-card");
+  });
+}
